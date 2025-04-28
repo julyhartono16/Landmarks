5d38b09822fe4e95d2f79ab1f3a31c11bcece6c7
July Hartono <julyhartono16@gmail.com>
2025-04-28 21:39:18 +0800

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by July Hartono on 28/04/25.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
